35d0ba21800e1954d09611eff1bf43f66e30369c
haran2231 <haran2231@gmail.com>
2024-08-12 22:27:51 +0530

    b

--- backend/server.js
+++ backend/server.js
@@ -32,19 +32,9 @@ app.use(bodyParser.json());
 app.use(cookieParser()); // Handle cookies
 
 // CORS Middleware
-const allowedOrigins = ['https://e-commerce-eight-jade.vercel.app'];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true, // if you need to allow cookies to be sent with requests
+  origin: 'https://e-commerce-eight-jade.vercel.app',
+  credentials: true, // If you need to allow credentials (cookies, authorization headers, etc.)
 }));
 
 
