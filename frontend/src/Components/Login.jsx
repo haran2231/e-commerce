7f4e859bf38eb9302227041b71e7e04580cfeeca
haran2231 <haran2231@gmail.com>
2024-08-12 21:53:37 +0530

    s

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
                 localStorage.setItem('userId', userId);
                 // localStorage.setItem('token', token); // If you use a token for authentication
     
-                // navigate('/home');
+                navigate('/home');
             }
         } catch (error) {
             console.error('Login failed', error);
