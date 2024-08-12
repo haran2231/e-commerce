f31f621efab689b87646bb464bed7af63c3e0a2b
haran2231 <haran2231@gmail.com>
2024-08-12 14:05:27 +0530

    z

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const authenticateJWT = (req, res, next) => {
-    // Check for token in cookies or headers
-    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+// const authenticateJWT = (req, res, next) => {
+//     // Check for token in cookies or headers
+//     const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
     
-    if (!token) return res.status(401).json(
-        // console.log("access denied"),
-        { message: 'Access denied' });
+//     if (!token) return res.status(401).json(
+//         // console.log("access denied"),
+//         { message: 'Access denied' });
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json(
-            console.log("Invalid token"),
-            { message: 'Invalid token' });
-        req.user = user;
-        next();
-    });
-};
+//     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+//         if (err) return res.status(403).json(
+//             console.log("Invalid token"),
+//             { message: 'Invalid token' });
+//         req.user = user;
+//         next();
+//     });
+// };
 
-module.exports = { authenticateJWT };
+// module.exports = { authenticateJWT };
